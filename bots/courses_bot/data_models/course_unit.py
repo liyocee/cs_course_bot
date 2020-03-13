6ccdff0128718635eed8883601c7426b7ceb5ed4
Collins Liyosi <collinskivale@gmail.com>
2020-03-13 11:45:22 +0300

    Add course exam_schedule attribute to courses
    - Add exam schedule attribute to courses

--- bots/courses_bot/data_models/course_unit.py
+++ bots/courses_bot/data_models/course_unit.py
@@ -5,10 +5,11 @@ from bots.courses_bot.data_models.lecture_hall import LectureHall
 
 
 class CourseUnit:
-    def __init__(self, name: str, code: str, lecture_hall: LectureHall):
+    def __init__(self, name: str, code: str, lecture_hall: LectureHall, exam_schedule: str):
         self.name: str = name
         self.code: str = code
         self.lecture_hall: LectureHall = lecture_hall
+        self.exam_schedule = exam_schedule
 
     @classmethod
     def create(cls, lecture_halls: List[LectureHall]):
@@ -17,6 +18,7 @@ class CourseUnit:
             return cls(
                 name=course_unit["name"],
                 code=course_unit["code"],
+                exam_schedule=course_unit["exam_schedule"],
                 lecture_hall=LectureHall.search_by_code(lecture_halls, course_unit["lecture_hall"])
             )
         return apply
